96c84906cba2829196570839e615d4f60f354709
bill-orange <billwebb12@gmail.com>
2025-03-08 06:58:43 -0800

    Update SprinklerController.ino
    Minors

--- SprinklerController.ino
+++ SprinklerController.ino
@@ -17,14 +17,23 @@
            Can not print to SD because of pin duplication
 02/25/2025 Improved download speed in support_functions.h revised
 02/26/2025 Clarify rain/sun if statement
+03/02/2025 Move DC pin from GPIO2 to GPIO19 to free up SD card I/O
+           Added SD card support of storing AI messages. Untested!
+03/03/2025 Added uSD Card loading graphics
+           Added Generalized Graphics function
+03/02/2025 Minors
+03/02/2025 Minors
+03/07/2025 Minors
 
 */
 #define USE_LINE_BUFFER  // Enable for faster rendering
-#define debug true         // eliminates print statements
-#include <WiFi.h>  // Needed for Version 3 Board Manager
+#define debug true       // eliminates print statements
+#include <WiFi.h>        // Needed for Version 3 Board Manager
 #include <WiFiMulti.h>
 #include <TFT_eSPI.h>  // Hardware-specific library
 //#include <SPI.h>            // needs testing may not be needed
+#include "FS.h"             // For SD Card
+#include "SD.h"             // For SD Card
 TFT_eSPI tft = TFT_eSPI();  // For Display
 
 #include <HTTPClient.h>         // Get Web Data
@@ -33,6 +42,8 @@ TFT_eSPI tft = TFT_eSPI();  // For Display
 #include <weathercall.h>  // Get Open WeatherMap data
 #include <ChatGPTuino.h>  // For AI Support
 #include "secrets.h"      //  Just what the name implies plus location info
+
+SPIClass hspi(HSPI);
 
 SET_LOOP_TASK_STACK_SIZE(12 * 1024);  // needed to handle really long strings
 
@@ -46,9 +57,26 @@ String language = Language;  // See notes tab
 String jsonBuffer;           // Storage for JSON String test if needed
 char *AIShortReply = "                                                             ";
 
+// REASSIGN_PINS for uSD
+int sck = 14;
+int miso = 2;
+int mosi = 15;
+int cs = 13;
+
+/*  For reference only do not uncomment - pin assigments for TFT Display
+#define TFT_MISO 19
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   12  // Chip select control pin
+#define TFT_DC   19  // Data Command control pin
+#define TFT_RST   4  // Reset pin (could connect to RST pin)
+*/
+
 const int TOKENS = 174;             // How lengthy a response you want, every token is about 3/4 a word
 const int NUM_MESSAGES = 30;        // Another budget limit
 const char *model = "gpt-4o-mini";  // OpenAI Model being used
+//const char *model = "gpt-4o";  // OpenAI Model being used
+const char *filename = "/AI_data.txt";
 
 weatherData w;                                     // Instance of OpenWeatherMap retriever
 Weathercall weather(apiKeyOpenWeather, location);  // Setup fetch
@@ -57,6 +85,21 @@ Weathercall forecast(apiKeyOpenWeather, location, 1);
 ChatGPTuino chat{ TOKENS, NUM_MESSAGES };  // Will store and send your most recent messages (up to NUM_MESSAGES)
 WiFiMulti wifiMulti;                       // Constructor for multiple wifi ssid
 
+/*-------------------------------- Display Graphics ----------------------------------------*/
+void showGraphic(String(image), int(relayState)) {
+  uint32_t t = millis();
+
+  setPngPosition(0, 0);
+  String githubURL = GITHUBURL + image;
+  const char *URLStr = githubURL.c_str();
+  Serial.println(URLStr);
+  load_png(URLStr);
+  t = millis() - t;
+  Serial.print(t);
+  Serial.println(" ms to load URL");
+  updateRelay(relayState);
+}
+
 /*-------------------------------- Setup ------------------------------------*/
 void setup() {
 
@@ -64,20 +107,47 @@ void setup() {
   delay(20);
   Serial.print("compiler Version: ");
   Serial.println(__cplusplus);
+  pinMode(RELAY_PIN, OUTPUT);
+  updateRelay(1);
   tft.begin();
   tft.fillScreen(0);
   tft.setRotation(2);
   fileInfo();
   connectToWifiNetwork();
-  wiFiconnect();
   delay(3000);
-  WIP();
-  pinMode(RELAY_PIN, OUTPUT);
-  updateRelay(1);
   chat.init(chatGPT_APIKey, model);  // Initialize AI chat
+
+  hspi.begin(sck, miso, mosi, cs);
+  delay(2000);
+
+  // Initialize SD card
+  if (!SD.begin(cs, hspi)) {
+    Serial.println("SD card initialization failed!");
+    showGraphic("SD_No.png", 1);
+  } else {
+    Serial.println("SD card initialized.");
+    showGraphic("SD_Yes.png", 1);
+  }
+  delay(3000);
+  // Check if the file exists
+  if (SD.exists(filename)) {
+    Serial.println("File exists.");
+  } else {
+    Serial.println("File does not exist. Creating file...");
+    // Create the file
+    File file = SD.open(filename, FILE_WRITE);
+    if (file) {
+      Serial.println("File created.");
+      file.close();
+    } else {
+      Serial.println("Failed to create file.");
+    }
+  }
+  //showGraphic("WIP.png", 1);
+  showGraphic("wait_for_it.png", 1);
 }
 
-/*-------------------------------- Loop ------------------------------------*/
+/*------------------------------------------- Loop -----------------------------------------------*/
 void loop() {
   int consultAINeeded = 1;  // yes
 
@@ -87,20 +157,31 @@ void loop() {
   if (strstr(w.weather.c_str(), "ain") != NULL || strstr(w.weather1.c_str(), "ain") != NULL || strstr(w.weather2.c_str(), "ain") != NULL) {
     consultAINeeded = 0;
     Serial.println("RAIN ............ RAIN ............... RAIN  >>>> No forecast needed");
-    rain();
+    showGraphic("12.png", 0);
   }
   if (consultAINeeded == 1) {
+    showGraphic("AI_computer_eye.png", 1);
     String tempString = AIForecast();
     Serial.println("*****************************");
     Serial.println(tempString);
     Serial.println("*****************************");
 
-    if ( tempString.indexOf("NO") != -1) {
-      sun();
+    if (tempString.indexOf("NO") != -1) {
+      showGraphic("11.png", 1);
     } else {
-      rain();
+      showGraphic("12.png", 0);
     }
-    if (debug) Serial.println(AIExplain());
+
+    if (debug && consultAINeeded == 1) {
+      String tempString2 = AIExplain();
+      Serial.println(tempString2);
+      const char *cstr = tempString2.c_str();
+      appendFile(SD, filename, cstr);
+      appendFile(SD, filename, "\n\n");
+    }
+    if (debug && consultAINeeded == 0) {
+      appendFile(SD, filename, "rain ... rain ... rain \n\n");
+    }
   }
 
   yield();
@@ -109,7 +190,7 @@ void loop() {
     delay(TIME_Between_Weather_Calls);
   }  // Recycle on error
   else {
-    error();
+    showGraphic("error.png", 1);
     yield();
   }
 }
@@ -150,65 +231,10 @@ void connectToWifiNetwork() {
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
+    showGraphic("WiFiConnected.png", 1);
   }
 }
 
-/*-------------------------------- Display Rain Graphic ------------------------------------*/
-void rain() {
-  uint32_t t = millis();
-
-  setPngPosition(0, 0);
-  load_png("https://raw.githubusercontent.com/bill-orange/Sprinkler_Controller/master/data/12.png");
-  t = millis() - t;
-  Serial.print(t);
-  Serial.println(" ms to load URL");
-  updateRelay(0);
-}
-
-/*-------------------------------- Display Sun Graphic -------------------------------------*/
-void sun() {
-  uint32_t t = millis();
-
-  setPngPosition(0, 0);
-  load_png("https://raw.githubusercontent.com/bill-orange/Sprinkler_Controller/master/data/11.png");
-  t = millis() - t;
-  Serial.print(t);
-  Serial.println(" ms to load URL");
-  updateRelay(1);
-}
-
-/*-------------------------------- Display Error Graphic -------------------------------------*/
-void error() {
-  uint32_t t = millis();
-
-  setPngPosition(0, 0);
-  load_png("https://raw.githubusercontent.com/bill-orange/Sprinkler_Controller/master/data/error.png");
-  t = millis() - t;
-  Serial.print(t);
-  Serial.println(" ms to load URL");
-}
-
-/*-------------------------------- Display Connection Graphic -------------------------------------*/
-void wiFiconnect() {
-  uint32_t t = millis();
-
-  setPngPosition(0, 0);
-  load_png("https://raw.githubusercontent.com/bill-orange/Sprinkler_Controller/master/data/WiFiConnected.png");
-  t = millis() - t;
-  Serial.print(t);
-  Serial.println(" ms to load URL");
-}
-
-/*-------------------------------- Display Sun Graphic -------------------------------------*/
-void WIP() {
-  uint32_t t = millis();
-
-  setPngPosition(0, 0);
-  load_png("https://raw.githubusercontent.com/bill-orange/Sprinkler_Controller/master/data/WIP.png");
-  t = millis() - t;
-  Serial.print(t);
-  Serial.println(" ms to load URL");
-}
 /*-------------------------------- Gets Inhibit Criteria from Github -------------------------------------*/
 String criteria() {
   uint32_t t = millis();
@@ -268,7 +294,7 @@ void updateRelay(int yesNo) {
 /*----------------------------------- Send Explaination Prompt to AI ----------------------------------------*/
 String AIExplain() {
   // Create the structures that hold the retrieved weather
-  String realUserMessage = "Please explain how you arrived at your Yes/No answer. Keep the explaination under 40 words";
+  String realUserMessage = "Please explain how you arrived at your Yes/No answer. Keep the explaination under 40 words.  Preface your reply with the time and date taken form the current conditions JSON String. For this reply you are not bound by the Yes/No restriction";
 
   Serial.println(realUserMessage);  // This and the lines below taken from library example
   int str_len = realUserMessage.length() + 1;
@@ -292,6 +318,24 @@ void fileInfo() {  // uesful to figure our what software is running
   tft.drawString(__FILENAME__, 30, 1100);
   tft.drawString(__DATE__, 30, 140);
   tft.drawString(__TIME__, 120, 140);
-  delay(6000);
-  tft.fillScreen(TFT_BLACK);
+  delay(3000);
 }
+
+/*---------------------------- Append File  ----------------------------------------*/
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if (file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+/*--------------------------------------------------------------------------------------------------*/
